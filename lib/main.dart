74aea40f4c2b54077b835c2315c9faf8bf99f820
whjin <wuhuajin09@163.com>
2023-09-18 22:55:20 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '图标组件',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('使用第三方包示例'),
+    final title = "网格列表示例";
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
         ),
-        body: Center(
-          child: TextButton(
-              onPressed: () {
-                const url = 'https://www.baidu.com/';
-                launch(url);
-              },
-              child: Text('百度一下')),
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter += 10;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        //使用GridView.count构建网格
+        body: new GridView.count(
+          primary: false,
+          //四周增加一定的空隙
+          padding: const EdgeInsets.all(20.0),
+          crossAxisSpacing: 30.0,
+          //一行上放三列数据
+          crossAxisCount: 3,
+          //数据项 五行三列
           children: <Widget>[
-            const Text(
-              '点击按钮计数:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('第一行第一列'),
+            const Text('第一行第二列'),
+            const Text('第一行第三列'),
+            const Text('第二行第一列'),
+            const Text('第二行第二列'),
+            const Text('第二行第三列'),
+            const Text('第三行第一列'),
+            const Text('第三行第二列'),
+            const Text('第三行第三列'),
+            const Text('第二行第三列'),
+            const Text('第三行第一列'),
+            const Text('第三行第二列'),
+            const Text('第三行第三列'),
+            const Text('第三行第三列'),
+            const Text('第三行第三列'),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
