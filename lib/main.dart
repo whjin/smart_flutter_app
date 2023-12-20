c539107567f8a0b797ca90f11ced6715c6d141f4
whjin <wuhuajin09@163.com>
2023-12-20 23:14:52 +0800

    合并更新代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new DefaultTabControllerSample());
+}
 
-class MyApp extends StatelessWidget {
+class DefaultTabControllerSample extends StatelessWidget {
+  //选项卡数据
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: '选项卡一'),
+    Tab(text: '选项卡二'),
+  ];
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new MyHomePage(),
-      title: 'MaterialApp',
-      routes: {
-        '/first': (BuildContext context) => FirstPage(),
-        '/second': (BuildContext context) => SecondPage(),
-      },
-      initialRoute: '/first',
+      //用来组装TabBar及TabBarView
+      home: DefaultTabController(
+        length: myTabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            //添加导航栏
+            bottom: TabBar(
+              tabs: myTabs,
+            ),
+          ),
+          //添加导航视图
+          body: TabBarView(
+            children: myTabs.map((Tab tab) {
+              return Center(child: Text('123'));
+            }).toList(),
+          ),
+        ),
+      ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-
-}
-
-class FirstPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(
-        title:Text('第一页'),
-      ),
-      body: Center(
-        child: RaiseButton(),
-      ),
-    )
   }
 }
